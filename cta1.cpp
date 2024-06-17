c5f2cb1481bfd868fb149465760159aa6c272f99
Ahmad Siddiqui <ahmad.siddiqui024@gmail.com>
2024-06-16 20:50:55 -0700

    completed CTA1

--- cta1.cpp
+++ cta1.cpp
@@ -1,4 +1,8 @@
 #include <iostream>
 int main(){
-	std::cout << "Hello";
+	std::cout << "John" <<std::endl;
+	std::cout << "Smith" <<std::endl;
+	std::cout << "123 Internet Street" <<std::endl;
+	std::cout << "Whoville" <<std::endl;
+	std::cout << "12345" <<std::endl;
 }
